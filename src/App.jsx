57f7e46254af3cb9f8dc8fcc1717f21dbc87759c
Diego Cabre <diegocabre@gmail.com>
2023-09-24 19:40:22 -0300

    Inicio del Proyecto

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import RedesSociales from './components/RedesSociales'
+
+function App() {
+  return (
+    <>
+      <h1 className='titulo'>Crea una Cuenta</h1>
+      <RedesSociales />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/RedesSociales.jsx
@@ -0,0 +1,9 @@
+function RedesSociales() {
+    return (
+        <div>
+            <h1>Redes Sociales</h1>
+        </div>
+    )
+}
+
+export default RedesSociales
