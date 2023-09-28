3014025d0543422bb8272ef7bcd06cf2960351b5
Diego Cabre <diegocabre@gmail.com>
2023-09-28 00:55:44 -0300

    Detalles de css

--- src/components/RedesSociales.jsx
+++ src/components/RedesSociales.jsx
@@ -1,15 +1,18 @@
 export const RedesSociales = ({ facebook, github, instagram }) => {
+    const onClick = () => {
+        alert('En Construcción');
+    }
     return (
         <>
             <div className="redes">
                 <div className="card">
-                    <i className="fa-brands fa-facebook">{facebook}</i>
+                    <i className="fa-brands fa-facebook" onClick={onClick}>{facebook}</i>
                 </div>
                 <div className="card">
-                    <i className="fa-brands fa-github">{github}</i>
+                    <i className="fa-brands fa-github" onClick={onClick}>{github}</i>
                 </div>
                 <div className="card">
-                    <i className="fa-brands fa-instagram">{instagram}</i>
+                    <i className="fa-brands fa-instagram" onClick={onClick}>{instagram}</i>
                 </div>
             </div>
         </>
